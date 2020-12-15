4763f1e736336a4870ba30aeb7d7a4e53a6565e6
Sylvie-B <unsolo@hotmail.fr>
2020-12-15 16:43:17 +0100

    affichage titre + photo + texte

--- exercice1.js
+++ exercice1.js
@@ -1,4 +1,4 @@
-let voiture = {
+let Voiture = {
     "Nom" : "Eggo",
     "imgUrl" : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
     "nombresRoues" : 4,
@@ -10,6 +10,25 @@ let voiture = {
     "vitesseMaxi" : "120km/h"
 };
 
+let title = document.querySelector('header');
+title.style.fontSize = "2rem";
+title.innerHTML = Voiture.Nom + " par le constructeur " + Voiture.Contructeur;
 
+// image
+let frame = document.getElementById('frame');
+let picture = document.createElement('img');
+picture.src = Voiture.imgUrl;
+frame.appendChild(picture);
 
+// title
+let name = document.getElementById('name');
+name.style.fontSize = "2rem";
+name.innerHTML = Voiture.Nom;
+
+// description
+let wheel = document.getElementById('wheel');
+wheel.innerHTML = Voiture.nombresRoues;
+
+let color = document.getElementById('color');
+color.innerHTML = Voiture.Couleur;
 
