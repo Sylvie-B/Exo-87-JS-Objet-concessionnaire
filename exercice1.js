d3765590b34c0711e29eaca24e8307122a6be136
Sylvie-B <unsolo@hotmail.fr>
2020-12-16 09:16:29 +0100

    correction des majuscules dans l'obj

--- exercice1.js
+++ exercice1.js
@@ -1,13 +1,13 @@
 let Voiture = {
-    "Nom" : "Eggo",
-    "imgUrl" : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
-    "nombresRoues" : 4,
-    "Couleur" : "Marron",
-    "Contructeur" : "Citroen",
-    "Carburant" : "Solaire",
-    "nombrePortes" : 3,
-    "Autonomie" : "450km",
-    "vitesseMaxi" : "120km/h"
+    nom : "Eggo",
+    imgUrl : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
+    nombresRoues : 4,
+    couleur: "Marron",
+    contructeur : "Citroen",
+    carburant : "Solaire",
+    nombrePortes : 3,
+    autonomie : "450km",
+    vitesseMaxi : "120km/h"
 };
 
 let container = document.getElementById("container");
